c3bb5b1c410f52e987cd90616b73929280ea0ec7
Justin <justinrkinsman@gmail.com>
2022-09-26 11:20:13 -0300

    Set up firebase project

---
+++ fake-instagram/src/firebase.config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCRzMy0qDjTGhSeOil4dvEwlLzpTAa7raQ",
+  authDomain: "fakeinstagram-81277.firebaseapp.com",
+  projectId: "fakeinstagram-81277",
+  storageBucket: "fakeinstagram-81277.appspot.com",
+  messagingSenderId: "792059962777",
+  appId: "1:792059962777:web:665152bc72f61d059f5f08"
+};
+
+const app = initializeApp(firebaseConfig);
