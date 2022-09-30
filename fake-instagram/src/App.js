62c9c3a6d22bcd82a40d727fc0789985bc673e55
Justin <justinrkinsman@gmail.com>
2022-09-30 10:43:28 -0300

    Fix login error

--- fake-instagram/src/App.js
+++ fake-instagram/src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
 //import { HomePage } from './components/HomePage';
-
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
 import { 
   GoogleAuthProvider, 
   signInWithPopup, 
-  getAuth,  
+  getAuth, 
 } from 'firebase/auth';
+
+import firebase from 'firebase/compat/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCRzMy0qDjTGhSeOil4dvEwlLzpTAa7raQ",
+  authDomain: "fakeinstagram-81277.firebaseapp.com",
+  projectId: "fakeinstagram-81277",
+  storageBucket: "fakeinstagram-81277.appspot.com",
+  messagingSenderId: "792059962777",
+  appId: "1:792059962777:web:665152bc72f61d059f5f08"
+};
+    
+firebase.initializeApp(firebaseConfig);
 
 export async function signIn() {
   let provider = new GoogleAuthProvider()
