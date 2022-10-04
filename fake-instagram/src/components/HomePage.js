2f56a42be80a080665f7a354b649c1030053d418
Justin <justinrkinsman@gmail.com>
2022-10-04 09:13:08 -0300

    Add css styling to sign up page

--- fake-instagram/src/components/HomePage.js
+++ fake-instagram/src/components/HomePage.js
@@ -1,6 +1,7 @@
 import { Header } from "./Header";
 import { MainPageContent } from "./homePageComponents/MainPageContent";
 import { SignUpPage } from "./homePageComponents/sidebarComponents/SignUpPage";
+import "./HomePage.css"
 
 export function HomePage(){
     return(
